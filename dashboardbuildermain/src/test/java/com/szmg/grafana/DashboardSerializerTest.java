ad934118c64e8c71edd82bcee41dee450d5e1ffa
Máté Szvoboda <mate.szvoboda@gmail.com>
2017-05-06 22:09:13 +0100

    updated readme

--- dashboardbuildermain/src/test/java/com/szmg/grafana/DashboardSerializerTest.java
+++ dashboardbuildermain/src/test/java/com/szmg/grafana/DashboardSerializerTest.java
@@ -7,7 +7,10 @@ import com.szmg.grafana.domain.Target;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static com.szmg.grafana.domain.DomainFactories.newDashboard;
+import static com.szmg.grafana.domain.DomainFactories.newGraph;
 import static com.szmg.grafana.domain.DomainFactories.newRow;
 import static com.szmg.grafana.domain.DomainFactories.newSingleStat;
 import static com.szmg.grafana.domain.DomainFactories.newTarget;
@@ -22,8 +25,9 @@ public class DashboardSerializerTest {
      */
     @Test
     @Ignore
-    public void printTestDashboard() {
-        System.out.println(serializer.toString(testDashboard()));
+    public void printTestDashboard() throws IOException {
+        //System.out.println(serializer.toString(testDashboard()));
+        serializer.write(testDashboard(), System.out);
     }
 
     @Test
@@ -32,6 +36,7 @@ public class DashboardSerializerTest {
         GrafanaEndpoint endpoint = new GrafanaEndpoint();
         endpoint.setBaseUrl(System.getProperty("grafana.url"));
         endpoint.setApiKey(System.getProperty("grafana.apiKey"));
+//        endpoint.setSessionCookie("");
         endpoint.setSkipSSLValidation(true);
         DashboardUploader uploader = new DashboardUploader(endpoint);
         uploader.upload(DashboardSerializerTest.testDashboard(), true);
@@ -39,25 +44,34 @@ public class DashboardSerializerTest {
     }
 
     public static Dashboard testDashboard() {
-        Target target = newTarget().withTarget("maxSeries(humidity.peti.test.sensors)");
+        Target target = newTarget()
+                .withTarget("maxSeries(humidity.peti.test.sensors)");
 
-        Row row1 = newTallRow()
+        Row row1 = newRow()
+                .withHeight("100px")
+                .addPanel(newSingleStat()
+                        .withTitle("Single stat test")
+                        .addTarget(target)
+                        .withSpan(2))
                 .addPanel(newText()
                         .withContent("<div class=\"text-center dashboard-header\"><span>This is the test</span></div>")
-                        .withMode("html").withTransparent(true).withSpan(8))
-                .addPanel(newSingleStat().withTitle("Single stat test").addTarget(target).withSpan(4));
+                        .withMode("html")
+                        .withTransparent(true)
+                        .withSpan(8))
+                .addPanel(newSingleStat()
+                        .withTitle("Single stat test")
+                        .addTarget(target)
+                        .withSpan(2));
 
-        Row row2 = newTallRow()
-                .addPanel(new Graph().addTarget(target).withSpan(12));
+        Row row2 = newRow()
+                .addPanel(newGraph()
+                        .addTarget(target)
+                        .withSpan(12));
 
         return newDashboard()
                 .withTitle("Test dashboard")
                 .addRow(row1)
                 .addRow(row2);
-    }
-
-    private static Row newTallRow() {
-        return newRow().withHeight("200px");
     }
 
 }
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 # Grafana dashboard builder in Java
 
 ```
-This Work in progress
+This is Work in progress
 ```
 
 Code your Grafana dashboards in Java, more or less type-safe, with auto-complete.
@@ -9,11 +9,94 @@ Code your Grafana dashboards in Java, more or less type-safe, with auto-complete
 E.g.,
 
 ```java
+//import static com.szmg.grafana.domain.DomainFactories.*
 
-// And here comes the code example... :)
+Target target = newTarget()
+        .withTarget("maxSeries(humidity.peti.test.sensors)");
+
+Row row1 = newRow()
+        .withHeight("100px")
+        .addPanel(newSingleStat()
+                .withTitle("Single stat test")
+                .addTarget(target)
+                .withSpan(2))
+        .addPanel(newText()
+                .withContent("<div class=\"text-center dashboard-header\"><span>This is the test</span></div>")
+                .withMode("html")
+                .withTransparent(true)
+                .withSpan(8))
+        .addPanel(newSingleStat()
+                .withTitle("Single stat test")
+                .addTarget(target)
+                .withSpan(2));
+
+Row row2 = newRow()
+        .addPanel(newGraph()
+                .addTarget(target)
+                .withSpan(12));
+
+Dashboard dashboard = newDashboard()
+        .withTitle("Test dashboard")
+        .addRow(row1)
+        .addRow(row2);
 
 ```
 
-License is MIT
+#### Higer level bits and bobs
+
+TODO: implement :)
+
+#### To String/Stream
+
+Convert to string or write onto a stream:
+
+```java
+DashboardSerializer serializer = new DashboardSerializer();
+
+// to String
+System.out.println(serializer.toString(dashboard));
+
+// write to Stream
+serializer.write(testDashboard(), System.out);
+```
+
+#### Upload it to Grafana
+
+It's easy to update your dashboards from code.
+
+TODO: create maven plugin for upload
+
+```java
+
+// Create an endpoint first...
+// Can use either API key or session cookie auth.
+GrafanaEndpoint endpoint = new GrafanaEndpoint();
+endpoint.setBaseUrl("https://grafana.mydomain.com");
+
+// see http://docs.grafana.org/http_api/auth/
+endpoint.setApiKey("some api key");
+
+// copy "grafana_sess" cookie
+//endpoint.setSessionCookie("123456789asd");
+
+// for insecure Grafana installations...
+endpoint.setSkipSSLValidation(true);
+
+
+// ...then the uploader...
+DashboardUploader uploader = new DashboardUploader(endpoint);
+
+
+// ...and upload, overwriting any existing dashboard with
+// that title. ("Test dashboard")
+uploader.upload(dashboard, true);
+
+
+```
+
+
+#### License
+
+MIT
 
 Copyright (c) 2017 Mate Gabor Szvoboda
